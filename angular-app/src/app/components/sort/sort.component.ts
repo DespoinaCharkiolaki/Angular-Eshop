be07121c62fe79f8580d5e99ccce49cbb1a7cb50
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@gmail.com>
2019-01-20 22:56:37 +0200

    Fix on compilation warnings

--- angular-app/src/app/components/sort/sort.component.ts
+++ angular-app/src/app/components/sort/sort.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sort',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SortComponent implements OnInit {
 
-  constructor() { }
+  sortType: string = 'Ascending';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSort() {
+  }
 }
